d6e25496b027d473b6b38e42780bbad765460197
Bhavesh <bhavesh953475@gmail.com>
2022-12-31 20:13:59 +0530

    changes added

--- src/utils/Contants.js
+++ src/utils/Contants.js
@@ -1,4 +1,4 @@
-export const SET_TOKEN = "SET_TOKEN";
+export const SET_TOKEN = "SET_TOKEN"
 export const SET_PLAYLISTS = "SET_PLAYLISTS";
 export const SET_USER = "SET_USER";
 export const SET_PLAYLIST = "SET_PLAYLIST";
